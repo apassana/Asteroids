f44871aa30b1d69caf7d0c5fe4c3b2598e238c04
apassana <apassana@uoregon.edu>
2025-09-29 19:57:38 -0700

    Added player movement

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import player as PlayerClass
 from constants import *
 
 def main():
     test = pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = PlayerClass.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
     print(f"{test[0]} tests passed \n{test[1]} tests failed")
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -13,7 +17,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill('black')
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
